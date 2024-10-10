e6b22c51b416b3e042ea97179dabb5303149700a
Avinash174 <avinashmagar15@gmail.com>
2024-10-10 18:15:34 +0530

    first commit

---
+++ hello_world.js
@@ -0,0 +1,3 @@
+console.log("Hello All");
+console.log(2+3);
+console.log("IIT I am Comming");
---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("Welcome In JavaScript");
+console.log(5+3);
+
+//declaration
+
+let name='Avinash';
+let numberIsEven=2;
+let isTrue=true;
+let rate=33.33;
+
+// typeof function
+
+console.log(typeof(name));
+console.log(typeof(numberIsEven));
+console.log(typeof(isTrue));
+console.log(typeof(rate));
+
+//operator
+
+let firstNumber=5;
+let seconNumber=8;
+
+gre=firstNumber<seconNumber;
+less=firstNumber>seconNumber;
+eq=firstNumber==seconNumber;
+th=firstNumber===seconNumber;
+
+console.log(gre);
+console.log(less);
+console.log(eq);
+
+// increment & decrement
+// firstNumber++;
+console.log(firstNumber++);
+console.log(++seconNumber);
+
+// bitwise - operator 
+
+and=firstNumber & seconNumber;
+console.log(and);
+
+or=firstNumber | seconNumber;
+console.log(or);
+
+
+
+
