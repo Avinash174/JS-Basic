5fe0d50b5dd53051622221f05e113a15de846ae0
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-10-14 11:06:35 +0530

    new  changes done

---
+++ switch.js
@@ -0,0 +1,17 @@
+// switch case
+
+let role="Developer";
+
+switch(role){
+
+    case 'Developer':
+        console.log('I am Developer');
+        break;
+    
+    case 'Testor':
+        console.log('I Am Testor');
+        break;
+    default:
+        console.log('My Role Not Define');
+
+}
