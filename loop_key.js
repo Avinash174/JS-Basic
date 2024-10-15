1d62db6d2b1ba9f68f685c45a30199bd6b5934bc
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-10-15 10:18:17 +0530

    new changes done

--- loop_key.js
+++ loop_key.js
@@ -4,12 +4,12 @@
 
 // break key word
 let i=1;
-// while(i<10){
-//      if (i===7)
-//         break
-//      console.log(i);
-//      i++;
-// }
+while(i<10){
+     if (i===7)
+        break
+     console.log(i);
+     i++;
+}
 
 // continue
 
