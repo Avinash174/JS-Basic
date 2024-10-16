a204139c361f35094ad7caba9556c944da308bb6
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-10-16 11:46:12 +0530

    new changes done

--- string.js
+++ string.js
@@ -1,1 +1,15 @@
 let a='Avinash Magar';
+
+
+// Templete Literal
+
+let b='Avinash\nMagar';
+
+// Date 
+
+const now =new Date();
+const date1 = new Date('May 11 2027 09:00')
+const date2 = new Date(2012,4,11,9);
+
+now.setFullYear(2024);
+
