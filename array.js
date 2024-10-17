5f906c750da4255045ad0bfdbd66f735c951b400
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-10-17 16:34:10 +0530

    new changes done

--- array.js
+++ array.js
@@ -2,58 +2,69 @@
 
 // adding element
 
-const number=[3,4];
+const number = [3, 4];
 
-number.push(5,6);
+number.push(5, 6);
 
 // Beginers
 
-number.unshift(1,2);
+number.unshift(1, 2);
 
-number.splice(2,2,"avinash")
+number.splice(2, 2, "avinash");
 
-
-// finding element 
+// finding element
 
 console.log(number.indexOf(2));
-console.log("\n")
+console.log("\n");
 console.log(number);
 
-console.log(number.lastIndexOf('avinash'));
-
+console.log(number.lastIndexOf("avinash"));
 
 // Reference Type
 
+// Removing element
 
-// Removing element 
+const a = [1, 2, 3, 4, 5, 6];
 
-const a=[1,2,3,4,5,6];
-
-const pop=a.pop();
+const pop = a.pop();
 
 console.log(a);
 
 // emptying an array
 
-let n=[1,2,3,4,5,6,7];
-let another=n;
+let n = [1, 2, 3, 4, 5, 6, 7];
+let another = n;
 
-another=[];
+another = [];
 
 console.log(another);
 
 // iterate Array
 
-const numbers = [1, 2,3, 4,5];
+const numbers = [3, 8, 7, 1, 5];
 
 // Using a for...of loop
 for (let number1 of numbers) {
-    console.log(number1);
+  console.log(number1);
 }
 
 // Using the forEach method correctly
-numbers.forEach(number1 => console.log(number1));
+numbers.forEach((number1) => console.log(number1));
 
+// Join Array
+
+const joined=numbers.join('*');
+console.log(joined);
+
+// sort 
+
+numbers.sort()
+console.log(numbers);
+
+// reverse 
+
+numbers.reverse()
+console.log(numbers);
 
 
 
